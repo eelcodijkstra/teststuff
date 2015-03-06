f7808c6caa490b8f5b9ffca19f0680262d4f556f
gregnordin <nordin@byu.edu>
2015-03-06 15:00:27 -0700

    update colors to use 0..1 range and not 0..255, which is wrong

--- pyqtgraph_tests/150303_slider_control_polarization.py
+++ pyqtgraph_tests/150303_slider_control_polarization.py
@@ -123,12 +123,12 @@ pts_e_z0 = np.dot(pts_e_z0, rot_efield_coord)
 pts_arrow = np.array( [[0.0, 0.0, 0.0], pts_e_z0[len(pts_e_z0)/2.0]] )
 
 # Get ready to make plots
-efield_color = (255, 0, 0, 255)
-efield_color_z0 = (255, 255, 255, 255)
-efield_color_arrow = (255, 255, 255, 255)
+efield_color = (1, 0, 0, 1)
+efield_color_z0 = (1, 1, 1, 1)
+efield_color_arrow = (1, 1, 1, 1)
 linewidth = 2.0
 linewidth2Dpol = 2.0
-linewidth2Defieldvector = 7.0
+linewidth2Defieldvector = 10.0
 
 # Make plots
 plt_e = gl.GLLinePlotItem(pos=pts_e, mode='line_strip', color=efield_color, width=linewidth, antialias=True)
